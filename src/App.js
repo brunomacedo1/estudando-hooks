9e155b31c93fe1693a69e7e525c25ae96bedeca4
Bruno <brunomacedo_31@hotmail.com>
2021-03-31 19:10:08 -0300

    Nagivation sem react-router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Accordion from './components/Accordion/Accordion';
 import Search from './components/Search/Search';
 import Dropdown from './components/Dropdown/Dropdown'
 import Translate from './components/Translate/Translate';
+import Route from './components/Route';
+import Header from './components/Header/Header';
 
 const items = [
   { title: 'O que é o React?',
@@ -32,10 +34,32 @@ const options = [
   },
 ];
 
+
+
 export default () => {
+  const [selected, setSelected] = useState(options[0])
+
   return (
-    <div style={{display: "flex", flexDirection:"column"}}>
-      <Translate/>
+    <div>
+      <Header/>
+      <Route path="/">
+        <Accordion items={items}/>
+      </Route>
+      <Route path="/list">
+        <Search/>
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown 
+          label="Selecione uma Cor"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 }
